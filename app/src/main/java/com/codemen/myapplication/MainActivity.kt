ddcd47ea700ed69f0acd7d01209c9b2ac89c971a
erixjavier <mendozaeria@globalhitss.com>
2020-03-23 17:52:18 -0500

    uodate of the coding

--- app/src/main/java/com/codemen/myapplication/MainActivity.kt
+++ app/src/main/java/com/codemen/myapplication/MainActivity.kt
@@ -7,6 +7,7 @@ import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 
@@ -17,21 +18,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val rollBotton:Button = this.findViewById(R.id.roll_button)
-        rollBotton.setOnClickListener { rollDice() }
-
-
-
-
+        roll_button.setOnClickListener { rollDice() }
     }
 
     private fun rollDice(){
-
-       // Toast.makeText(this, "button clicked",
-           // Toast.LENGTH_SHORT).show()
         val randomInt = Random().nextInt(6) + 1
-        val diceImage: ImageView = findViewById(R.id.dice_image)
+        val diceImage: ImageView = dice_image
         val drawableResource = when (randomInt) {
             1 -> R.drawable.dice_1
             2 -> R.drawable.dice_2
